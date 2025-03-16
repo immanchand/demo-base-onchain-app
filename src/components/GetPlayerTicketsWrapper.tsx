ea9e47adc0067f3b2ffbc781f1eee4b2c5185f83
immanchand <53471578+immanchand@users.noreply.github.com>
2025-03-16 17:04:56 +0400

    Update GetPlayerTicketsWrapper.tsx

--- src/components/GetPlayerTicketsWrapper.tsx
+++ src/components/GetPlayerTicketsWrapper.tsx
@@ -1,18 +1,21 @@
 'use client';
 import { createPublicClient, http } from 'viem';
-import type { Address } from 'viem';
+import type { Address, Hex } from 'viem';
 import { baseSepolia } from 'viem/chains';
 import {
-    BASE_SEPOLIA_CHAIN_ID,
     contractABI,
     contractAddress,
   } from '../constants';
 import React, { useEffect, useState } from 'react';
+import { useAccount } from 'wagmi';
 
 
-export default function GetPlayerTicketsWrapper({ address }: { address: Address }) {
+export default function GetPlayerTicketsWrapper({ tickets }: { tickets: number }) {
 
-    const [playerTickets, setPlayerTickets] = useState<number | null>(null);
+    const { address } = useAccount();
+
+    const [playerTickets, setPlayerTickets] = useState<number>(0);
+
 
     useEffect(() => {
 
@@ -27,7 +30,7 @@ export default function GetPlayerTicketsWrapper({ address }: { address: Address 
             address: contractAddress,
             abi: contractABI,
             functionName: 'getTickets',
-            args: [address],
+            args: [address as Hex],
           });
 
           setPlayerTickets(Number(playerTickets));
@@ -36,15 +39,10 @@ export default function GetPlayerTicketsWrapper({ address }: { address: Address 
         fetchPlayerTickets();
       }, []);
 
+
     return (
-      <div 
-      className="flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[url('../svg/TicketsBg.jpg')] bg-cover bg-center"
-      >
-        <div className="rounded-xl bg-[#F3F4F6] px-4 py-[11px]">
-          <p className="font-normal text-indigo-600 text-xl not-italic tracking-[-1.2px]">
-            Tickets: {playerTickets}
-          </p>
-        </div>
-      </div>
+        <span >
+            {playerTickets + tickets}
+        </span>
     );
   }

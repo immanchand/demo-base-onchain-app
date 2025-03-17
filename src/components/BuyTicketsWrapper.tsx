dfa15ae08a919b240786a2e0e659a6104802208c
immanchand <53471578+immanchand@users.noreply.github.com>
2025-03-17 12:22:42 +0400

    Update BuyTicketsWrapper.tsx

--- src/components/BuyTicketsWrapper.tsx
+++ src/components/BuyTicketsWrapper.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import {
   Transaction,
   TransactionButton,
@@ -11,11 +10,8 @@ import {
   TransactionToastIcon,
   TransactionToastLabel,
 } from '@coinbase/onchainkit/transaction';
-import type {
-  TransactionError,
-  TransactionResponse,
-} from '@coinbase/onchainkit/transaction';
-import { encodeFunctionData, Hex } from 'viem'
+import type { TransactionError, TransactionResponse } from '@coinbase/onchainkit/transaction';
+import { encodeFunctionData, Hex } from 'viem';
 import {
   BASE_SEPOLIA_CHAIN_ID,
   contractABI,
@@ -25,22 +21,21 @@ import {
 import { QuantitySelector } from '../../node_modules/@coinbase/onchainkit/esm/internal/components/QuantitySelector';
 import { useState } from 'react';
 
-
-
-export default function BuyTicketsWrapper({ updateTickets }: { updateTickets: Function }) {
-  
+export default function BuyTicketsWrapper({ updateTickets }: { updateTickets: (newTickets: number) => void }) {
   const [quantity, setQuantity] = useState<number>(1);
-  
+
   const data = encodeFunctionData({
     abi: contractABI,
     functionName: 'mintTickets',
   });
 
-  const calls = [{
-    to: contractAddress as Hex,
-    data: data as Hex,
-    value: BigInt(GAME_PRICE_WEI * quantity),
-  }];
+  const calls = [
+    {
+      to: contractAddress as Hex,
+      data: data as Hex,
+      value: BigInt(GAME_PRICE_WEI * quantity),
+    },
+  ];
 
   const handleError = (err: TransactionError) => {
     console.error('Transaction error:', err);
@@ -48,36 +43,37 @@ export default function BuyTicketsWrapper({ updateTickets }: { updateTickets: Fu
 
   const handleSuccess = (response: TransactionResponse) => {
     console.log('Transaction successful', response);
-    updateTickets(quantity);
+    updateTickets(1); // Trigger refresh in parent one time
   };
-
+  
   return (
-    
     <div className="flex w-[450px]" style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-
-      <div >
-      <QuantitySelector
-        className="mt-0 mr-auto ml-auto w-[450px] max-w-full text-[white]"
-        onChange={(value: string) => setQuantity(Number(value))}
-        minQuantity={1}
-        maxQuantity={100}
-        placeholder="" />
+      <div>
+        <QuantitySelector
+          className="mt-0 mr-auto ml-auto w-[450px] max-w-full text-[white]"
+          onChange={(value: string) => setQuantity(Number(value))}
+          minQuantity={1}
+          maxQuantity={100}
+          placeholder=""
+        />
       </div>
       <Transaction
         calls={calls}
         className="w-[450px]"
         chainId={BASE_SEPOLIA_CHAIN_ID}
         onError={handleError}
-        onSuccess={handleSuccess} >
+        onSuccess={handleSuccess}
+      >
         <TransactionButton
           className="mt-0 mr-auto ml-auto w-[450px] max-w-full text-[white]"
-          text='Buy Tickets'/>
+          text="Buy Tickets"
+        />
         <TransactionStatus>
           <TransactionStatusLabel />
           <TransactionStatusAction />
         </TransactionStatus>
         <TransactionToastAction />
-        <TransactionToast >
+        <TransactionToast>
           <TransactionToastIcon />
           <TransactionToastLabel />
         </TransactionToast>

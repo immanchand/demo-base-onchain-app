5a8fa45ec2fe214c373a9a9f6e981430e9594ed9
immanchand <53471578+immanchand@users.noreply.github.com>
2025-03-17 10:42:49 +0400

    Update page.tsx

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,1 +1,17 @@
+'use client';
+import Navbar from 'src/components/Navbar';
 
+
+
+export default function Games() {
+
+
+  return (
+    <div className="flex h-full w-96 max-w-full flex-col px-1 md:w-[1008px] rounded-xl">
+      <Navbar />
+      <section className="templateSection flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-gray-100 px-2 py-4 md:grow">
+        <p>EMPTY GAMES PAGE</p>
+      </section>
+    </div>
+  );
+}

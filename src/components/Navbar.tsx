f7b56913a4707a0f828266002e134c5cea88b4b2
immanchand <53471578+immanchand@users.noreply.github.com>
2025-03-18 14:27:14 +0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,68 +3,50 @@ import ArcadeCasinoSvg from '../svg/ArcadeCasinoSvg';
 import { useAccount } from 'wagmi';
 import LoginButton from '../components/LoginButton';
 import Link from 'next/link';
-import { useCallback, useState } from 'react';
-import GetPlayerTicketsWrapper from './GetPlayerTicketsWrapper';
+import { useTicketContext } from '../context/TicketContext';
+import React from 'react';
 
-
-const Navbar = () => {
-
+const Navbar = React.memo(() => {
   const { address } = useAccount();
-  const [ticketCount, setTicketCount] = useState<number>(0);
+  const { ticketCount } = useTicketContext();
 
-  // Handle ticket count update from GetPlayerTicketsWrapper
-  const handleTicketsUpdate = (totalTickets: number) => {
-    setTicketCount(totalTickets);
-  };
-  
   return (
-        <nav className="bg-slate-100 border-b-2 border-b-slate-300 rounded-xl">
-            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                <div className="relative flex h-16 items-center justify-between">
-                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                        <div className="flex flex-shrink-0 items-center">
-                        <a
-                        title="Arcade Casino"
-                        target="_blank"
-                        rel="noreferrer"
-                        >
-                        <ArcadeCasinoSvg />
-                        </a>
-                        </div>
-                        <div className="hidden sm:ml-6 sm:block">
-                            <div className="flex space-x-4">
-                                <Link href={'/games'} >
-                                    <p className="rounded-md px-3 py-2 text-l font-large font-bold text-slate-700">Play & Win</p>
-                                </Link>
-                                {address && ( 
-                                    <Link href="/tickets">
-                                        <p className="rounded-md px-3 py-2 text-l font-large font-bold text-slate-700">Tickets</p>
-                                    </Link>
-                                )}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <div className="flex items-center gap-3">
-                            {address && (
-                            <div
-                                className="w-10 h-10 bg-gray-500 rounded-full flex items-center justify-center text-xl font-bold text-white">
-                                {ticketCount}
-                            </div>
-                            )}
-                            <LoginButton />
-                        </div>
-                    </div>
-                </div>
+    <nav className="bg-slate-100 border-b-2 border-b-slate-300 rounded-xl">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-shrink-0 items-center">
+              <a title="Arcade Casino" target="_blank" rel="noreferrer">
+                <ArcadeCasinoSvg />
+              </a>
             </div>
-
-        {/* Fetch ticket count, re-mount when refreshKey changes */}
-        <GetPlayerTicketsWrapper
-                refreshKey={0} //no need to refresh
-                onTicketsUpdate={handleTicketsUpdate}
-              />
-        </nav>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                <Link href={'/games'}>
+                  <p className="rounded-md px-3 py-2 text-l font-large font-bold text-slate-700">Play & Win</p>
+                </Link>
+                {address && (
+                  <Link href="/tickets">
+                    <p className="rounded-md px-3 py-2 text-l font-large font-bold text-slate-700">Tickets</p>
+                  </Link>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <div className="flex items-center gap-3">
+              {address && (
+                <div className="w-10 h-10 bg-gray-500 rounded-full flex items-center justify-center text-xl font-bold text-white">
+                  {ticketCount}
+                </div>
+              )}
+              <LoginButton />
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
-};
+});
 
 export default Navbar;

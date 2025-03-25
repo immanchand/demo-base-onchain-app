790ff8e90213368eb1b427a074bf8d4f490e31a3
immanchand <53471578+immanchand@users.noreply.github.com>
2025-03-25 15:02:34 +0400

    Update LoginButton.tsx
    style change to black yellow and white

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -4,8 +4,8 @@ import WalletWrapper from './WalletWrapper';
 export default function LoginButton() {
   return (
     <WalletWrapper
-      className="min-w-[90px]"
-      text="Log In"
+      className="min-w-[90px] button bg-yellow-500 text-white rounded-xl hover:bg-black hover:text-yellow-500 border-2 border-yellow-500 disabled:bg-yellow-500 disabled:text-white"
+      text="log in"
       withWalletAggregator={true}
     />
   );

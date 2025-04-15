22df72e0ad7d82617622cfe1b2afd04d23e268aa
immanchand <53471578+immanchand@users.noreply.github.com>
2025-04-15 12:26:44 +0400

    Update WalletWrapper.tsx
    minor ui green identity change

--- src/components/WalletWrapper.tsx
+++ src/components/WalletWrapper.tsx
@@ -38,7 +38,7 @@ export default function WalletWrapper({
           <Name />
         </ConnectWallet>
         <WalletDropdown>
-          <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick={true}>
+          <Identity className="px-4 pt-3 pb-2 bg-green-400" hasCopyAddressOnClick={true}>
             <Avatar />
             <Name />
             <Address />

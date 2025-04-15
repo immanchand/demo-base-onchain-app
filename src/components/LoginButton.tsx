549d7cbab37c81a8d78accc59e7a950553d908b2
immanchand <53471578+immanchand@users.noreply.github.com>
2025-04-15 12:24:24 +0400

    Update LoginButton.tsx
    global css changes

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -4,7 +4,7 @@ import WalletWrapper from './WalletWrapper';
 export default function LoginButton() {
   return (
     <WalletWrapper
-      className="min-w-[90px] button bg-yellow-500 text-white hover:bg-green-400 hover:text-yellow-500 border-2 border-yellow-500 disabled:bg-yellow-500 disabled:text-white"
+      className="btn-primary min-w-[90px]"
       text="LOG IN"
       withWalletAggregator={true}
     />

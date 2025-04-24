2fbbee95fbb23d8fa4e426d44e10c74fab798244
immanchand <53471578+immanchand@users.noreply.github.com>
2025-04-24 15:13:45 +0400

    Update route.js

--- src/app/api/server/route.js
+++ src/app/api/server/route.js
@@ -324,7 +324,7 @@ export async function POST(request) {
               return new Response(JSON.stringify({ status: 'error', message: 'Suspicious deltaTime variance' }), { status: 400 });
             }
             //all games check total telemetry frame delta time against stats.time/1000=gameTimeSec
-            if (totalFrameDeltaTime > gameTimeSec || totalFrameDeltaTime < gameTimeSec * 0.98) {
+            if (totalFrameDeltaTime > gameTimeSec  * 1.001 || totalFrameDeltaTime < gameTimeSec * 0.999) {
               console.log('Frame delta time and stats total time mismatch', {
                 address,
                 gameId,

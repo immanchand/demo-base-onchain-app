d0055bdbdf626fe15dec0f7e4dfb47dbe2305502
immanchand <53471578+immanchand@users.noreply.github.com>
2025-05-05 10:15:01 +0400

    Update Jump.tsx

--- src/components/Jump.tsx
+++ src/components/Jump.tsx
@@ -300,10 +300,15 @@ const Jump: React.FC<JumpProps> = ({ gameId, existingHighScore, updateTickets })
         let maxObstacles = 0;
         const update = (deltaTime: number) => {
             const elapsedTime = (performance.now() - startTimeRef.current) / 1000;
+            const difficultyFactor = Math.min(elapsedTime / JUMP_PARAMETERS.DIFFICULTY_FACTOR_TIME, 1);
+            const clusterChance = difficultyFactor * JUMP_PARAMETERS.CLUSTER_CHANCE;
             const timeLevel = Math.floor(elapsedTime / 8);
             const speedMultiplier = 1 + timeLevel * 0.05;
-            const obstacleSpeed = JUMP_PARAMETERS.BASE_OBSTACLE_SPEED * speedMultiplier;
-            const minGap = JUMP_PARAMETERS.OBSTACLE_SIZE * (50 - Math.min(timeLevel, 10) * 4);
+            //const obstacleSpeed = JUMP_PARAMETERS.BASE_OBSTACLE_SPEED * speedMultiplier;
+            const obstacleSpeed = JUMP_PARAMETERS.BASE_OBSTACLE_SPEED * (1 + difficultyFactor);
+            //const minGap = JUMP_PARAMETERS.OBSTACLE_SIZE * (50 - Math.min(timeLevel, 10) * 4);
+            //const minGap2 = JUMP_PARAMETERS.OBSTACLE_SIZE * 10 * (1 - difficultyFactor);
+            const minGap = JUMP_PARAMETERS.OBSTACLE_SIZE * 5 * Math.min((10 - (difficultyFactor*10)),1);
             const obstacleSize = JUMP_PARAMETERS.OBSTACLE_SIZE;
 
             cloudOffset += 0.3 * deltaTime * 60; //sky speed 0.3
@@ -373,6 +378,9 @@ const Jump: React.FC<JumpProps> = ({ gameId, existingHighScore, updateTickets })
 
             const rightmostObstacle = obstaclePool.reduce((max, obs) => Math.max(max, obs.x), -minGap);
             if (canvas.width - rightmostObstacle >= minGap && !gameOver) {
+
+                
+
                 obstaclePool.push(...spawnObstacles(canvas, obstacleSpeed, elapsedTime, frameCount));
                 //lastObstacleSpawnX = canvas.width; ??
             }

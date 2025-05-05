dae4d50106eb5287ee13ce1f77030c0542087dfc
immanchand <53471578+immanchand@users.noreply.github.com>
2025-05-05 11:59:52 +0400

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -13,28 +13,26 @@ export const FLY_PARAMETERS = {
   SHIP_HEIGHT: 40 * (3/4),
   OBSTACLE_SIZE: 50,
   BASE_OBSTACLE_SPEED: -3, // Higher (e.g., -4) = faster obstacles, harder; Lower (e.g., -2) = easier
-  MIN_SPAWN_INTERVAL: 150, // Lower (e.g., 200) = more frequent obstacles, harder; Higher (e.g., 500) = easier
-  MAX_SPAWN_INTERVAL: 1000, // starting frequency of obstacles in ms
+  MIN_SPAWN_INTERVAL: 200, // ms Lower (e.g., 200) = more frequent obstacles, harder; Higher (e.g., 500) = easier
+  MAX_SPAWN_INTERVAL: 1000, // ms starting frequency of obstacles
   GRAVITY: 0.2, // Higher (e.g., 0.3) = faster fall, harder; Lower (e.g., 0.1) = easier
   FLAP_VELOCITY: -5, // Higher (e.g., -6) = stronger flaps, easier; Lower (e.g., -4) = harder
   SCORE_MULTIPLIER: 100,
-  DIFFICULTY_FACTOR_TIME: 30, // 90 seconds till max difficulty
-  CLUSTER_CHANCE: 0.5,
+  DIFFICULTY_FACTOR_TIME: 90, // 90 seconds till max difficulty
 };
 export const JUMP_PARAMETERS = {
   SHIP_WIDTH: 40 * (3/4),
   SHIP_HEIGHT: 50,
   OBSTACLE_SIZE: 50,
   BASE_OBSTACLE_SPEED: -3, // Higher (e.g., -5) = faster obstacles, harder; Lower (e.g., -2) = easier
-  MIN_SPAWN_INTERVAL: 300, // Lower (e.g., 200) = tighter gaps, harder; Higher (e.g., 400) = easier
-  MAX_SPAWN_INTERVAL: 1000, // starting frequency of obstacles in ms
+  MIN_SPAWN_INTERVAL: 300, // px Lower (e.g., 200) = tighter gaps, harder; Higher (e.g., 400) = easier
+  MAX_SPAWN_INTERVAL: 1000, // px starting frequency of obstacles in ms
   GRAVITY: 0.4, // Higher (e.g., 0.5) = faster fall, harder; Lower (e.g., 0.3) = easier
   JUMP_VELOCITY: -12, // Higher (e.g., -14) = higher jumps, easier; Lower (e.g., -10) = harder
   SCORE_MULTIPLIER: 100,
   DIFFICULTY_FACTOR_TIME: 90, //seonds till max difficulty
   DOUBLE_PRESS_THRESHOLD: 300, // ms for double jump
   GROUND_HEIGHT_RATIO: 0.8,
-  CLUSTER_CHANCE: 0.5,
 };
 export const SHOOT_PARAMETERS = {
   SHIP_WIDTH: 30,

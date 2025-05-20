bd2c9e5ea38da4829db299ad626cd2ceeb92d0ff
immanchand <53471578+immanchand@users.noreply.github.com>
2025-05-20 10:20:15 +0400

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -17,7 +17,7 @@ export const FLY_PARAMETERS = {
   GRAVITY: 0.2, // Higher (e.g., 0.3) = faster fall, harder; Lower (e.g., 0.1) = easier
   FLAP_VELOCITY: -5, // Higher (e.g., -6) = stronger flaps, easier; Lower (e.g., -4) = harder
   SCORE_MULTIPLIER: 100,
-  DIFFICULTY_FACTOR_TIME: 90, // 90 seconds till max difficulty
+  DIFFICULTY_FACTOR_TIME: 120, // 90 seconds till max difficulty
   CLUSTER_CHANCE: 0.5,
 };
 export const JUMP_PARAMETERS = {
@@ -25,12 +25,12 @@ export const JUMP_PARAMETERS = {
   SHIP_HEIGHT: 50,
   OBSTACLE_SIZE: 50,
   BASE_OBSTACLE_SPEED: -3, // Higher (e.g., -5) = faster obstacles, harder; Lower (e.g., -2) = easier
-  MIN_SPAWN_INTERVAL: 800, // ms Lower (e.g., 400) = tighter gaps, harder; Higher (e.g., 800) = easier
+  MIN_SPAWN_INTERVAL: 1000, // ms Lower (e.g., 400) = tighter gaps, harder; Higher (e.g., 800) = easier
   MAX_SPAWN_INTERVAL: 4200, // ms starting frequency of obstacles in ms
   GRAVITY: 0.4, // Higher (e.g., 0.5) = faster fall, harder; Lower (e.g., 0.3) = easier
   JUMP_VELOCITY: -11, // Higher (e.g., -14) = higher jumps, easier; Lower (e.g., -10) = harder
   SCORE_MULTIPLIER: 100,
-  DIFFICULTY_FACTOR_TIME: 90, //seonds till max difficulty
+  DIFFICULTY_FACTOR_TIME: 120, //seonds till max difficulty
   DOUBLE_PRESS_THRESHOLD: 400, // ms for double jump
   GROUND_HEIGHT_RATIO: 0.8,
   CLUSTER_CHANCE: 0.8,

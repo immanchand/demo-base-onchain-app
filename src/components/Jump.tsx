196b82c7b9555c6c1743ef7bffa6ce294709add9
immanchand <53471578+immanchand@users.noreply.github.com>
2025-05-21 14:40:53 +0400

    Update Jump.tsx

--- src/components/Jump.tsx
+++ src/components/Jump.tsx
@@ -684,7 +684,7 @@ const Jump: React.FC<JumpProps> = ({ gameId, existingHighScore, updateTickets })
                     )}
                 </div>
             ) : (
-                <div ref={containerRef} className="w-full max-w-[1008px] h-[80vh] min-h-[400px] min-w-[300px] relative">
+                <div ref={containerRef} className="w-full max-w-[1008px] min-w-[300px] h-[80vh] max-h-[900px] min-h-[400px] relative">
                     <div className="text-primary-text mb-1 text-center font-mono">
                         <span className="text-2xl text-accent-yellow">SCORE: {Math.floor(score)}</span>
                         <span className="text-2xl text-accent-yellow ml-8">TOP SCORE: {existingHighScore}</span>

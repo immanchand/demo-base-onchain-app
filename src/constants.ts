6a108def579d2fa246826b1c21e2e22383c3a227
immanchand <53471578+immanchand@users.noreply.github.com>
2025-05-22 23:02:06 +0400

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -11,8 +11,8 @@ export const scaleBaseH = 500;
 export const minScale = 0.8; // min scale for canvas
 // Difficulty parameters
 export const FLY_PARAMETERS = {
-  SHIP_WIDTH: 40,
-  SHIP_HEIGHT: 40 * (3/4),
+  SHIP_WIDTH: 50,
+  SHIP_HEIGHT: 50 * (3/4),
   OBSTACLE_SIZE: 50,
   BASE_OBSTACLE_SPEED: -3, // Higher (e.g., -4) = faster obstacles, harder; Lower (e.g., -2) = easier
   MIN_SPAWN_INTERVAL: 200, // ms Lower (e.g., 200) = more frequent obstacles, harder; Higher (e.g., 500) = easier
